4a5b9a0de8205d94616e31e16266f376a8703071
Ezaak250 <157524562+Ezaak250@users.noreply.github.com>
2024-01-28 17:42:49 -0800

    Review.G

---
+++ Review Project.G/app.js
@@ -0,0 +1,102 @@
+// local reviews data
+const reviews = [
+  {
+    id: 1,
+    name: 'Kyle Mabini',
+    job: 'Boarder patrol Officer',
+    img: "https://i.imgur.com/u8qFeX9.jpg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: 'Jackson Ponto',
+    job: 'welder',
+    img: "https://i.imgur.com/b4PLLXE.jpg",
+    text: 'Helvetica artisan  thundercats blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'Bruce Wane',
+    job: 'Superhero',
+    img: "https://i.imgur.com/INzjlPR.jpg",
+    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'Jungkook',
+    job: 'Kpop Idol',
+    img: "https://i.imgur.com/Vnp9z9Q.jpg",
+    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+];
+// select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+const jumpToStartBtn = document.querySelector('.start-of-selection');
+const jumpToEndBtn = document.querySelector('.end-of-selection');
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener('DOMContentLoaded', function () {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+});
+
+// show person based on item
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+// show next person
+nextBtn.addEventListener('click', function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+// show prev person
+prevBtn.addEventListener('click', function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
+// show random person
+randomBtn.addEventListener('click', function () {
+  console.log('hello');
+
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson(currentItem);
+});
+
+//Jump to Start
+
+jumpToStartBtn.addEventListener('click', function(){
+  currentItem = 0;
+  showPerson(currentItem);
+
+})
+
+
+// Jump to End
+jumpToEndBtn.addEventListener('click', function(){
+  currentItem = 3;
+  showPerson(currentItem);
+
+})
